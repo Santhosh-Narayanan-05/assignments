e894047ab5ddf6465c9eace96d42750a855bfd66
Santhosh-Narayanan-05 <swasan05@gmail.com>
2026-03-17 16:41:52 +0530

    Initial commit

---
+++ tests/datatypes.js
@@ -0,0 +1,21 @@
+let UserName="SANTHOSH"
+console.log(UserName);
+console.log(typeof UserName);
+
+let organization=" CISSOL SOFTWARE TECHNOLOGIES PRIVATE LIMITED"
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumber=8220901476
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+let knowsAutomation=true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+
