f250d28ee12d1484bfee0f02cb77af589dbceb8f
Santhosh-Narayanan-05 <swasan05@gmail.com>
2026-03-17 16:51:11 +0530

    Org name change

--- tests/datatypes.js
+++ tests/datatypes.js
@@ -2,7 +2,7 @@ let UserName="SANTHOSH"
 console.log(UserName);
 console.log(typeof UserName);
 
-let organization=" CISSOL SOFTWARE TECHNOLOGIES PRIVATE LIMITED"
+let organization=" CISSOL SOFTWARE TECHNOLOGIES PVT LTD"
 console.log(organization);
 console.log(typeof organization);
 
